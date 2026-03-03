28d84c42dd2ded1c7153fce2ea4137ecedf0e67d
gibbyeight <samgibson8@gmail.com>
2026-03-03 13:42:23 +0000

    adding test ci

---
+++ addons/gbsn_core_mechanics/test/event_bus/event_bus_service_test.gd
@@ -0,0 +1,13 @@
+# GdUnit generated TestSuite
+class_name EventBusServiceTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source: String = 'res://addons/gbsn_core_mechanics/event_bus/event_bus_service.gd'
+
+
+func test_subscribe() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
